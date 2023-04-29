a97a4adda4454a8a0b5daa8847a3537c71c1e118
Patrick Shiel <p.shiel@live.co.uk>
2023-04-29 19:02:25 +0100

    fix: prisma TS def

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
