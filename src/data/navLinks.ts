cad17439afa592f5f97648fda840225e082a993d
Patrick Shiel <p.shiel@live.co.uk>
2023-05-01 20:40:46 +0100

    feat: Ordered links

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -9,16 +9,19 @@ const navLinks: navLinkData[] = [
   {
     name: "Search",
     icon: faMagnifyingGlass,
+    mobileOrder: 1,
     path: "/",
   },
   {
     name: "Settings",
     icon: faSliders,
+    mobileOrder: 0,
     path: "/search-settings",
   },
   {
     name: "My NPCs",
     icon: faHelmetBattle,
+    mobileOrder: 2,
     path: "/my-npcs",
   },
 ];
@@ -28,5 +31,6 @@ export default navLinks;
 export interface navLinkData {
   name: string;
   icon: IconDefinition;
+  mobileOrder: number;
   path: string;
 }
