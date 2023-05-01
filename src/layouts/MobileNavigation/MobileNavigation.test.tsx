167f1aefb5ea1e2f6f1d1710ce4fbce979e845ba
Patrick Shiel <p.shiel@live.co.uk>
2023-05-01 19:47:06 +0100

    improvement: Streamline test setup

--- src/layouts/MobileNavigation/MobileNavigation.test.tsx
+++ src/layouts/MobileNavigation/MobileNavigation.test.tsx
@@ -7,25 +7,23 @@ import { MobileNavigation, MobileNavigationStyles } from ".";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
+beforeEach(() => {
+  mockRouter.push("/");
+  render(<MobileNavigation />);
+});
+
 describe("Mobile navigation", () => {
   it("renders a link for each nav item", () => {
-    mockRouter.push("/");
-    render(<MobileNavigation />);
     const links = screen.getAllByRole("link");
     expect(links).toHaveLength(navLinks.length);
   });
 
   it("renders an icon in each link", () => {
-    mockRouter.push("/");
-    render(<MobileNavigation />);
     const svgs = screen.getAllByTestId("nav-link-icon");
     expect(svgs).toHaveLength(navLinks.length);
   });
 
   it("renders regular styling for link to other pages", () => {
-    mockRouter.push("/");
-    render(<MobileNavigation />);
-
     const link = screen.getByText("Settings").closest("a");
     if (!!link)
       expect(link.classList.contains(MobileNavigationStyles.current)).toBe(
@@ -34,9 +32,6 @@ describe("Mobile navigation", () => {
   });
 
   it("renders different styling for the current link to the current page", () => {
-    mockRouter.push("/");
-    render(<MobileNavigation />);
-
     const link = screen.getByText("Search").closest("a");
     if (!!link)
       expect(link.classList.contains(MobileNavigationStyles.current)).toBe(
