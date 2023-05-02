8265847a73fa66ec50f53a90dff7095cf48d977e
Patrick Shiel <p.shiel@live.co.uk>
2023-05-02 23:32:57 +0100

    feat: Main nav page stubs

---
+++ src/pages/my-npcs.tsx
@@ -0,0 +1,9 @@
+const MyNpcsPage = () => {
+  return (
+    <div>
+      <h1>My NPCs</h1>
+    </div>
+  );
+};
+
+export default MyNpcsPage;
---
+++ src/pages/search-settings.tsx
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>Search settings</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
