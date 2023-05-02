15d126a8ca2eda6c67765098e45438e3ae7165af
Patrick Shiel <p.shiel@live.co.uk>
2023-05-02 21:21:15 +0100

    improvement: ButtonProps from interface to type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./Button.module.scss";
 
+/* -------------------------------------------------------------------------- */
+/*                            Base button component                           */
+/* -------------------------------------------------------------------------- */
+
 const Button = ({ type = "button", ...props }: ButtonProps) => {
   return <button className={styles.base} {...props} />;
 };
 
 export default Button;
 
-interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {}
+type ButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
