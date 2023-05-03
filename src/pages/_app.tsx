b3bc601b464bce07effbaf61d1ada29959f19c32
Patrick Shiel <p.shiel@live.co.uk>
2023-05-03 23:07:33 +0100

    fix: NextJS global css bug workaround

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import MainLayout from "@/layouts/Main/Main";
+// Load global stylesheet first to avoid NextJS bug where global styles override
+// component styles.
+//
+// See https://github.com/vercel/next.js/issues/16630#issuecomment-1225265272
 import "@/styles/globals.scss";
+
+import MainLayout from "@/layouts/Main/Main";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
